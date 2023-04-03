8b5b59491230f4cf51789936687650cc2c51ec7c
Curtis Slone <curtisdslone@gmail.com>
2023-04-03 10:53:46 +0200

    Updated Bastion use case resource dependencies

--- bastion-keyvault-ssh-linux-vm/resource.dependencies.tf
+++ bastion-keyvault-ssh-linux-vm/resource.dependencies.tf
@@ -8,8 +8,8 @@
 resource "azurerm_virtual_network" "hub-vnet" {
   name = "hub-vm-network"
   location = var.location
-  resource_group_name = module.mod_linux_vm.linux_vm_resource_group_name
-  address_space = ["10.0.0.0/16"]
+  resource_group_name = module.mod_bastion_rg.name
+  address_space = ["10.1.0.0/16"]
   tags = {
     environment = "test"
   }
@@ -17,9 +17,9 @@ resource "azurerm_virtual_network" "hub-vnet" {
 
 resource "azurerm_subnet" "hub-snet" {
     name = "hub-vm-subnet"
-    resource_group_name = module.mod_linux_vm.linux_vm_resource_group_name
-    virtual_network_name = azurerm_virtual_network.jump-vnet.name
-    address_prefixes = ["10.0.1.0/24"]
+    resource_group_name = module.mod_bastion_rg.name
+    virtual_network_name = azurerm_virtual_network.hub-vnet.name
+    address_prefixes = ["10.1.1.0/24"]
 }
 
 #
