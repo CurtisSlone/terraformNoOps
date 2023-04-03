c8d4f655b4f51249ee2b7086fd99980ec31ed015
Curtis Slone <curtisdslone@gmail.com>
2023-04-03 08:31:22 +0200

    Updated base files

---
+++ bastion-keyvault-ssh-linux-vm/data.tf
@@ -0,0 +1,29 @@
+data "azurenoopsutils_resource_name" "vnet" {
+    name = var.workload_name
+    resource_type = "azurerm_virtual_network"
+    prefixes = [var.org_name, var.location]
+    suffixes = compact([var.environment])
+    use_slug = true
+    clean_input = true
+    separator = "-"
+}
+
+data "azurenoopsutils_resource_name" "subnet" {
+    name = var.workload_name
+    resource_type = "azurerm_subnet"
+    prefixes = [var.org_name, var.location]
+    suffixes = compact([var.environment])
+    use_slug = true
+    clean_input = true
+    separator = "-"
+}
+
+data "azurenoopsutils_resource_name" "law" {
+    name = var.workload_name
+    resource_type = "azurerm_log_analytics_workspace"
+    prefixes = [var.org_name, var.location]
+    suffixes = compact([var.environment])
+    use_slug = true
+    clean_input = true
+    separator = "-"
+}
