be52722fa2882c3e9059a7dae011739acec3a292
Curtis Slone <curtisdslone@gmail.com>
2023-04-03 15:32:51 +0200

    Error Record

--- bastion-keyvault-hub/resources.keyvault.tf
+++ bastion-keyvault-hub/resources.keyvault.tf
@@ -33,6 +33,11 @@ resource "azurerm_key_vault_access_policy" "keyvault-access-policy" {
         ]
 }
 
+#  Error: checking for presence of existing Secret "bastion-ssh-key-private" (Key Vault "https://hub-keyvault-testing-123.vault.azure.net/"): keyvault.BaseClient#GetSecret: Failure responding to request: StatusCode=403 -- Original Error: autorest/azure: Service reticrosoft.com/fwlink/?linkid=2125287" InnerError={"code":"AccessDenied"}
+# │
+# │   with azurerm_key_vault_secret.bastion_ssh_key_private,
+# │   on resources.keyvault.tf line 36, in resource "azurerm_key_vault_secret" "bastion_ssh_key_private":
+# │   36: resource "azurerm_key_vault_secret" "bastion_ssh_key_private" {
 resource "azurerm_key_vault_secret" "bastion_ssh_key_private" {
   name = "bastion-ssh-key-private"
   value = tls_private_key.bastion_ssh_key.private_key_pem
