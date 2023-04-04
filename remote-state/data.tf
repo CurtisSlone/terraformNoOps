6142ee1925b99cb0d37f4c2cc99a4e9ebfc19159
Curtis Slone <curtisdslone@gmail.com>
2023-04-04 15:42:47 +0200

    Updated remote-state data

--- remote-state/data.tf
+++ remote-state/data.tf
@@ -2,7 +2,7 @@ data "azurerm_client_config" "current" {}
 
 data "azurerm_key_vault" "remote-backend-keyvault" {
     name = azurerm_key_vault.remote-backend-keyvault.name
-    resource_group_name = module.mod_network_artifacts_rg.name
+    resource_group_name = module.mod_network_artifacts_rg.resource_group_name
 }
 
 data "azurerm_key_vault_secret" "storage_account_key" {
