ed62696e7131db7bdf4ab831be93c1a1dafb19d3
Curtis Slone <curtisdslone@gmail.com>
2023-04-06 14:22:51 +0200

    Added versions to examples

---
+++ remote-state-spn-github-actions/example/versions.tf
@@ -0,0 +1,23 @@
+terraform {
+    required_version = ">= 1.2"
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~> 3.22"
+    }
+    azurenoopsutils = {
+        source = "azurenoops/azurenoopsutils"
+        version = "~> 1.0.4"
+    }
+  }
+  backend "azurerm" {
+    storage_account_name = ""
+    container_name = ""
+    key = "terraform.tfstate"
+    access_key = ""
+  }
+}
+
+provider "azurerm" {
+    features {}
+}
