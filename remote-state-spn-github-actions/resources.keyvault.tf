238df5336802effa528d6bd9affa4575bae7151e
Curtis Slone <curtisdslone@gmail.com>
2023-04-09 22:17:00 +0200

    Removed actions

--- remote-state-spn-github-actions/resources.keyvault.tf
+++ remote-state-spn-github-actions/resources.keyvault.tf
@@ -6,48 +6,12 @@ resource "azurerm_key_vault" "tfstatekv" {
   tenant_id = data.azurerm_client_config.current.tenant_id
   enabled_for_disk_encryption = false
   purge_protection_enabled = false
-}
-
-resource "azurerm_key_vault_access_policy" "spnpolicy" {
-  
-    key_vault_id = azurerm_key_vault.tfstatekv.id
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = azuread_service_principal.GHASPN.id
+  access_policy {
     
-   key_permissions = [
-        "Get",
-        "List",
-        "Create",
-        "Delete",
-        "Purge"
-    ]
-    secret_permissions = [
-        "Get",
-        "List",
-        "Set",
-        "Delete",
-        "Purge",
-        "Recover"
-
-        ]
-
-        storage_permissions = [
-        "Get",
-        "GetSAS",
-        "SetSAS",
-        "Delete",
-        "Purge"
-
-        ]
-}
-
-resource "azurerm_key_vault_access_policy" "current" {
-  
-    key_vault_id = azurerm_key_vault.tfstatekv.id
     tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azuread_client_config.current.object_id
-
-     key_permissions = [
+    object_id = data.azurerm_client_config.current.object_id
+    
+    key_permissions = [
         "Get",
         "List",
         "Create",
@@ -72,13 +36,11 @@ resource "azurerm_key_vault_access_policy" "current" {
         "Purge"
 
         ]
+  }
 }
+
 
 resource "azurerm_key_vault_secret" "tfstatekvsecret" {
-  depends_on = [
-    azurerm_key_vault_access_policy.current,
-    azurerm_key_vault_access_policy.spnpolicy
-  ]
   name = "tfstatesakey"
   value = azurerm_storage_account.tfstatesa.primary_access_key
   key_vault_id = azurerm_key_vault.tfstatekv.id
